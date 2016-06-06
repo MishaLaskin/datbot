6d531ed1006005c5a9566ea9804433046f00e189
Michael Laskin <laskin.misha@gmail.com>
2016-06-05 22:34:38 -0400

    a

--- server.js
+++ server.js
@@ -21,3 +21,22 @@ controller.setupWebserver(port, function (err, webserver) {
     console.log('Ready')
   })
 })
+
+// user said hello
+controller.hears(['hello'], 'message_received', function(bot, message) {
+
+    bot.reply(message, 'Hey there.');
+
+});
+
+controller.hears(['cookies'], 'message_received', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.say('Did someone say cookies!?!!');
+        convo.ask('What is your favorite type of cookie?', function(response, convo) {
+            convo.say('Golly, I love ' + response.text + ' too!!!');
+            convo.next();
+        });
+    });
+});
